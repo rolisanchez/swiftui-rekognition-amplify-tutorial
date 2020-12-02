129f603c93b54231b82a973648b1b0d5874e2b41
rolisanchez <roli.s91@gmail.com>
2020-12-02 13:16:07 -0300

    Start project

---
+++ SwiftUIAmplifyRekognitionTutorial/SwiftUIAmplifyRekognitionTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAmplifyRekognitionTutorial
+//
+//  Created by Victor Rolando Sanchez Jara on 12/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAmplifyRekognitionTutorial/SwiftUIAmplifyRekognitionTutorial/SwiftUIAmplifyRekognitionTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAmplifyRekognitionTutorialApp.swift
+//  SwiftUIAmplifyRekognitionTutorial
+//
+//  Created by Victor Rolando Sanchez Jara on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAmplifyRekognitionTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
