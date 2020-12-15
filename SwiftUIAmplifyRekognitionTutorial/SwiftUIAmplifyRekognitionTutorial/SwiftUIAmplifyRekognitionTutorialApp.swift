056ccdcd145752b10951ed95be5a5c5850ab1d7e
rolisanchez <roli.s91@gmail.com>
2020-12-14 21:33:59 -0300

    Configure Project

--- SwiftUIAmplifyRekognitionTutorial/SwiftUIAmplifyRekognitionTutorial/SwiftUIAmplifyRekognitionTutorialApp.swift
+++ SwiftUIAmplifyRekognitionTutorial/SwiftUIAmplifyRekognitionTutorial/SwiftUIAmplifyRekognitionTutorialApp.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import Amplify
+//import AmplifyPlugins
+import AWSPredictionsPlugin
 
 @main
 struct SwiftUIAmplifyRekognitionTutorialApp: App {
+    // swiftlint:disable weak_delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    // swiftlint:enable weak_delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                        launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        print("didFinishLaunchingWithOptions")
+        // Initialize Amplify
+        initializeAWSAmplify()
+        return true
+    }
+
+    func initializeAWSAmplify() {
+        let predictionsPlugin = AWSPredictionsPlugin()
+        do {
+            try Amplify.add(plugin: predictionsPlugin)
+            try Amplify.configure()
+            print("Amplify initialized")
+        } catch {
+            fatalError("Failed to configure Amplify \(error)")
         }
     }
 }
